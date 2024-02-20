0c2c6715b6e53312b3cea3976568c423e3eade1e
Matthew Andres Moreno <mmore500.login@gmail.com>
2024-02-19 21:05:15 -0500

    Make wrappers forward seaborn return values

--- seaborn_polars/src/seaborn_polars.py
+++ seaborn_polars/src/seaborn_polars.py
@@ -32,96 +32,96 @@ def seaborn_polars(func: Callable[[Any], Any]):
 
 @seaborn_polars
 def relplot(data, *args, **kwargs):
-    sns.relplot(data, *args, **kwargs)
+    return sns.relplot(data, *args, **kwargs)
 
 @seaborn_polars
 def lineplot(data, *args, **kwargs):
-    sns.lineplot(data, *args, **kwargs)    
+    return sns.lineplot(data, *args, **kwargs)    
 
 @seaborn_polars
 def scatterplot(data, *args, **kwargs):
-    sns.scatterplot(data, *args, **kwargs)
+    return sns.scatterplot(data, *args, **kwargs)
     
 @seaborn_polars
 def displot(data, *args, **kwargs):
-    sns.displot(data, *args, **kwargs)
+    return sns.displot(data, *args, **kwargs)
     
 @seaborn_polars
 def histplot(data, *args, **kwargs):
-    sns.histplot(data, *args, **kwargs)
+    return sns.histplot(data, *args, **kwargs)
     
 @seaborn_polars
 def kdeplot(data, *args, **kwargs):
-    sns.kdeplot(data, *args, **kwargs)
+    return sns.kdeplot(data, *args, **kwargs)
     
 @seaborn_polars
 def ecdfplot(data, *args, **kwargs):
-    sns.ecdfplot(data, *args, **kwargs)
+    return sns.ecdfplot(data, *args, **kwargs)
     
 @seaborn_polars
 def rugplot(data, *args, **kwargs):
-    sns.rugplot(data, *args, **kwargs)
+    return sns.rugplot(data, *args, **kwargs)
     
 @seaborn_polars
 def catplot(data, *args, **kwargs):
-    sns.catplot(data, *args, **kwargs)
+    return sns.catplot(data, *args, **kwargs)
     
 @seaborn_polars
 def stripplot(data, *args, **kwargs):
-    sns.stripplot(data, *args, **kwargs)
+    return sns.stripplot(data, *args, **kwargs)
     
 @seaborn_polars
 def swarmplot(data, *args, **kwargs):
-    sns.swarmplot(data, *args, **kwargs)
+    return sns.swarmplot(data, *args, **kwargs)
     
 @seaborn_polars
 def boxplot(data, *args, **kwargs):
-    sns.boxplot(data, *args, **kwargs)
+    return sns.boxplot(data, *args, **kwargs)
     
 @seaborn_polars
 def violinplot(data, *args, **kwargs):
-    sns.violinplot(data, *args, **kwargs)
+    return sns.violinplot(data, *args, **kwargs)
     
 @seaborn_polars
 def boxenplot(data, *args, **kwargs):
-    sns.boxenplot(data, *args, **kwargs)
+    return sns.boxenplot(data, *args, **kwargs)
     
 @seaborn_polars
 def pointplot(data, *args, **kwargs):
-    sns.pointplot(data, *args, **kwargs)
+    return sns.pointplot(data, *args, **kwargs)
     
 @seaborn_polars
 def barplot(data, *args, **kwargs):
-    sns.barplot(data, *args, **kwargs)
+    return sns.barplot(data, *args, **kwargs)
     
 @seaborn_polars
 def countplot(data, *args, **kwargs):
-    sns.countplot(data, *args, **kwargs)
+    return sns.countplot(data, *args, **kwargs)
     
 @seaborn_polars
 def lmplot(data, *args, **kwargs):
-    sns.lmplot(data, *args, **kwargs)
+    return sns.lmplot(data, *args, **kwargs)
     
 @seaborn_polars
 def regplot(data, *args, **kwargs):
-    sns.regplot(data, *args, **kwargs)
+    return sns.regplot(data, *args, **kwargs)
     
 @seaborn_polars
 def residplot(data, *args, **kwargs):
-    sns.residplot(data, *args, **kwargs)
+    return sns.residplot(data, *args, **kwargs)
     
 @seaborn_polars
 def heatmap(data, *args, **kwargs):
-    sns.heatmap(data, *args, **kwargs)
+    return sns.heatmap(data, *args, **kwargs)
     
 @seaborn_polars
 def clustermap(data, *args, **kwargs):
-    sns.clustermap(data, *args, **kwargs)
+    return sns.clustermap(data, *args, **kwargs)
     
 @seaborn_polars
 def pairplot(data, *args, **kwargs):
-    sns.pairplot(data, *args, **kwargs)
+    return sns.pairplot(data, *args, **kwargs)
     
 @seaborn_polars
 def jointplot(data, *args, **kwargs):
-    sns.jointplot(data, *args, **kwargs)
+    return sns.jointplot(data, *args, **kwargs)
